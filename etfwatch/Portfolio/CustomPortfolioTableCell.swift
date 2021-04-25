b5da528e87da647a50236ee6cc02d47ea328b029
Aydin Tekin <aydin.tekin@ibsolution.de>
2021-04-25 13:43:53 +0200

    first work on new cells

---
+++ etfwatch/Portfolio/CustomPortfolioTableCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPortfolioTableCell.swift
+//  etfwatch
+//
+//  Created by Aydin Tekin on 25.04.21.
+//
+
+import Cocoa
+
+class CustomPortfolioTableCell: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
